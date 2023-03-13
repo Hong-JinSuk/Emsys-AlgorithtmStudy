ebaf21e6a252d32fe6c958f9cdf384439a6e1413
JaeChanHWANG <57826563+OopsIForgot@users.noreply.github.com>
2023-03-13 19:49:20 +0900

    Update solution_of_boj_12865.cpp
    This code is using less memory and less time
    It's more efficient

--- HwangJaechan/solution_of_boj_12865.cpp
+++ HwangJaechan/solution_of_boj_12865.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
-#include <utility>
 
 using namespace std;
-using the_thing = pair<int, int>; // weight,value 
+using things = pair<int, int>;
 
-int cupbag[100001];
-int resultbag[100001];
-the_thing things[100];
-
-void solution(int n, int maxweight) {
-	for (int i = 0; i < n; i++) {
-		for (int j = things[i].first; j <= maxweight; j++) {
-			resultbag[j] = max(cupbag[j], things[i].second + cupbag[j - things[i].first]);
-		}
-		for (int j = 1; j <= maxweight; j++) {
-			cupbag[j] = resultbag[j];
-		}
-	}
-}
+things item[100];
+int dp[100001];
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-	int n, k; // input
-	cin >> n >> k;
 	
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> item[i].first >> item[i].second;
 	for (int i = 0; i < n; i++) {
-		cin >> things[i].first >> things[i].second;
+		int w = item[i].first;
+		int val = item[i].second;
+		for (int j = m; j >=w; j--) {
+			dp[j] = max(dp[j], dp[j - w] + val);
+		}
 	}
 
-	solution(n, k);
-
-	cout << resultbag[k];
+	cout << dp[m];
 
 	return 0;
 }
