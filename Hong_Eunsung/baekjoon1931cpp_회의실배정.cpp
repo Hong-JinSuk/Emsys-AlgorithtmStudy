9092bcaf2e7328893cfb452bf76f06d73edeff6f
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2023-03-20 01:48:35 +0900

    solved.ac 클래스3 에센셜 문제 - dp를 이용한 풀이
    가독성을 위해 테스트 코드 삭제

--- Hong_Eunsung/baekjoon1931cpp_회의실배정.cpp
+++ Hong_Eunsung/baekjoon1931cpp_회의실배정.cpp
@@ -27,29 +27,12 @@ int main() {
 		cin >> meet[i].start >> meet[i].end;
 	}
 
-
-	//test code
-	cout << "meet : \n";
-	for (int i = 0; i < n; i++) {
-		cout << "(" << meet[i].start << ", " << meet[i].end << "), ";
-	}
-	cout << "\n";
-
-
 	sort(meet.begin(), meet.end(), compare);
 	latest = meet.back().end;
 	vector<int>dp(latest+1, 0);
 
 
-	//test code
-	cout << "meet : \n";
-	for (int i = 0; i < n; i++) {
-		cout << "(" << meet[i].start << ", " << meet[i].end << "), ";
-	}
-	cout << "\n";
-
-
-	//dp[0]¸¸ µû·Î ¼³Á¤
+	//dp[0]ë§Œ ë”°ë¡œ ì„¤ì •
 	dp[0] = 0;
 	temp = 0;
 	while (meet[temp].end == 0) {
@@ -57,25 +40,20 @@ int main() {
 		temp++;
 	}
 
-	int j = 1; //dp ¹øÈ£
-	for (int i = temp; i < n; i++) { //i¿¡ µû¶ó meet¸¦ ¾Õ¿¡¼­ºÎÅÍ Å½»ö
+	int j = 1; //dp ë²ˆí˜¸
+	for (int i = temp; i < n; i++) { //iì— ë”°ë¼ meetë¥¼ ì•žì—ì„œë¶€í„° íƒìƒ‰
 		//test code
 		cout << "i : " << i << "\n";
 
-		while (j < meet[i].end) {//´ÙÀ½À¸·Î °Ë»çÇÒ meetÀÇ Á¾·á½Ã°£ÀÌ ÇöÀç dp¹øÈ£º¸´Ù Å©´Ù¸é
+		while (j < meet[i].end) {//ë‹¤ìŒìœ¼ë¡œ ê²€ì‚¬í•  meetì˜ ì¢…ë£Œì‹œê°„ì´ í˜„ìž¬ dpë²ˆí˜¸ë³´ë‹¤ í¬ë‹¤ë©´
 			dp[j+1] = dp[j];
 			j++;
 		}
 
-		//test code
-		cout << "dp[meet[" << i << "].start : " << dp[meet[i].start] << "\n";
-
 		temp = dp[meet[i].start] + 1;
 		dp[j] = dp[j-1] < temp ? temp : dp[j-1];
 		max = dp[j] > max ? dp[j] : max;
-		
-		//test code
-		cout << "temp is : " << temp << " and " << "new dp[" << j << "] : " << dp[j] << "\n";
+
 	}
 	cout << max;
 }
