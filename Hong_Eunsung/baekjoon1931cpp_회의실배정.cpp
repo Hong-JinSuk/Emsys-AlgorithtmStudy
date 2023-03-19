caf057d08e41f8f969fb3b8f1d4c7d24aabd9f03
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2023-03-20 01:46:23 +0900

    solved.ac 클래스3 에센셜 문제 - dp를 이용한 풀이

---
+++ Hong_Eunsung/baekjoon1931cpp_회의실배정.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+struct meetTime {
+	int start;
+	int end;
+};
+
+bool compare(meetTime a, meetTime b) {
+	if (a.end == b.end)
+		return a.start < b.start;
+	else
+		return a.end < b.end;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, temp, max = 0, latest = 0;
+	cin >> n;
+	vector<meetTime> meet(n);
+	for (int i = 0; i < n; i++) {
+		cin >> meet[i].start >> meet[i].end;
+	}
+
+
+	//test code
+	cout << "meet : \n";
+	for (int i = 0; i < n; i++) {
+		cout << "(" << meet[i].start << ", " << meet[i].end << "), ";
+	}
+	cout << "\n";
+
+
+	sort(meet.begin(), meet.end(), compare);
+	latest = meet.back().end;
+	vector<int>dp(latest+1, 0);
+
+
+	//test code
+	cout << "meet : \n";
+	for (int i = 0; i < n; i++) {
+		cout << "(" << meet[i].start << ", " << meet[i].end << "), ";
+	}
+	cout << "\n";
+
+
+	//dp[0]만 따로 설정
+	dp[0] = 0;
+	temp = 0;
+	while (meet[temp].end == 0) {
+		dp[0]++;
+		temp++;
+	}
+
+	int j = 1; //dp 번호
+	for (int i = temp; i < n; i++) { //i에 따라 meet를 앞에서부터 탐색
+		//test code
+		cout << "i : " << i << "\n";
+
+		while (j < meet[i].end) {//다음으로 검사할 meet의 종료시간이 현재 dp번호보다 크다면
+			dp[j+1] = dp[j];
+			j++;
+		}
+
+		//test code
+		cout << "dp[meet[" << i << "].start : " << dp[meet[i].start] << "\n";
+
+		temp = dp[meet[i].start] + 1;
+		dp[j] = dp[j-1] < temp ? temp : dp[j-1];
+		max = dp[j] > max ? dp[j] : max;
+		
+		//test code
+		cout << "temp is : " << temp << " and " << "new dp[" << j << "] : " << dp[j] << "\n";
+	}
+	cout << max;
+}
