310ad6f818825ab910d413df292dd19259ba2f76
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2023-09-21 09:07:37 +0900

    Update 미술시간.cpp

--- HwangJaechan/SecondSemester/미술시간.cpp
+++ HwangJaechan/SecondSemester/미술시간.cpp
@@ -15,22 +15,22 @@ void paint(int a, int b, int c) {
 	vector<int>tmp;
 	int e = b;
 	for (int i = a; i <= b; i++) {
-		if (color[i]) {
+		if (color[i]) { // 색이 이미 칠해져있는 경우 
 			int t = i;
 			tmp.push_back(t);
-			while (t != goTo[t]) {
+			while (t != goTo[t]) { // 색이 칠해져있는 마지막까지 이동 
 				tmp.push_back(t);
 				t = goTo[t];
 			}
-			e = max(t, e);
-			i = t;
+			e = max(t, e); // 가장 마지막 색이 칠해져있는 곳 저장
+			i = t; // cursor이동 
 		}
-		else {
-			color[i] = c;
+		else { // 색이 칠해져있지 않은 경우 
+			color[i] = c;// 색칠함
 			tmp.push_back(i);
 		}
 	}
-	for (auto i : tmp) {
+	for (auto i : tmp) {//중간에 들른곳을 모두 마지막 e로 업데이트 
 		goTo[i] = e;
 	}
 }
